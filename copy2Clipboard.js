a4746c70a88db9c723b6145a24df3466d0d3ae12
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-03-27 00:21:33 +0530

    Added copy to clipboard
    Source Code: https://codepen.io/shaikmaqsood/pen/XmydxJ

---
+++ copy2Clipboard.js
@@ -0,0 +1,9 @@
+﻿// Copied from here: https://codepen.io/shaikmaqsood/pen/XmydxJ
+
+function copyPassword(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
