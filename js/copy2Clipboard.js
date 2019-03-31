fc2c50ba66147b543a3d6042dc2123326de78233
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-03-31 12:13:54 +0530

    Format the code. Added onload JS function.

--- js/copy2Clipboard.js
+++ js/copy2Clipboard.js
@@ -1,9 +1,8 @@
 ﻿// Copied from here: https://codepen.io/shaikmaqsood/pen/XmydxJ
-
 function copyPassword(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
