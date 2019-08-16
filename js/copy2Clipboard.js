61b51827656411f204a2314939addde4ca583921
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-08-16 11:56:48 +0530

    Test

--- js/copy2Clipboard.js
+++
@@ -1,8 +0,0 @@
-﻿// Copied from here: https://codepen.io/shaikmaqsood/pen/XmydxJ
-function copyPassword(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
